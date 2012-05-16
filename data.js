a4f002fc16acd5dc4e79e613eb0a11b75a543b25
Jason Shapiro <jason.g.shapiro@gmail.com>
2012-05-15 20:46:09 -0700

    Slider Up and Running

--- data.js
+++ data.js
@@ -6,12 +6,37 @@ var data1 = {"budget_line_info": {"prob_x": 0.5, "y_max": 200.0, "y_units": "dol
 var xdata = (data1.budget_line_info.x_max - data1.budget_line_info.x_min) * Math.random() + data1.budget_line_info.x_min
 var ydata = (data1.budget_line_info.y_max - data1.budget_line_info.y_min) * Math.random() + data1.budget_line_info.y_min
 
+// Plot Intialization
+
 $(function() {
 	
 	var placeholder = $('#placeholder');
 	
-	var data = [[xdata,0],[0,ydata]];
+	var data = [[xdata,0], [0,ydata]];
 	
-	$.plot(placeholder,[data]);
+	var plot = $.plot(placeholder,[data]);
 
 });
+
+// Slider Initialization
+
+$(function() {
+    	$("#sliderd").slider({ 
+    		min: 0,
+    		max: Math.floor(xdata*100)/100,
+    		step: .01,
+    		slide: function( event, ui ) {
+    				$( "#displayx" ).val( "$" + ui.value );
+    				$( "#displayy" ).val( "$" + Math.floor(100*(ydata - (ydata/xdata)*ui.value))/100 );
+    				
+    		} 
+    		});
+    		$( "#displayx" ).val( "$" + $( "#sliderd" ).slider( "value" ));
+    		$( "#displayy" ).val( "$" + Math.floor(100*ydata)/100 );
+
+});
+
+
+///var dataSet = [[xdata, 0], [20,30], [0, ydata]];
+///plot.setData([dataSet]);
+///plot.draw();
